302271f40cf850be71a707a69dd24361448cade5
deveripon <developershahadathussainripon@gmail.com>
2024-10-13 18:44:22 +0600

    feature: added vercel speed insight

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang='en'>
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                {children}
+                <SpeedInsights />
+            </body>
         </html>
     );
 }
