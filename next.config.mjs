76bd39644b60c1baf9febaaa5f8c5256d01c50af
Shahadat Hussain Ripon <developershahadathussainripon@gmail.com>
2025-07-24 15:25:28 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ import nextra from "nextra";
 const withNextra = nextra({
     theme: "nextra-theme-docs",
     themeConfig: "./theme.config.jsx",
+    defaultShowCopyCode: true,
 });
 
 const nextConfigFunction = async (phase) => {
